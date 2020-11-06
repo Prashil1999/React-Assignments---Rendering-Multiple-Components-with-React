984d04b4c28ef3d1f7150b6b9211d30cc259b39f
Prashil S Gupta <33949630+Prashil1999@users.noreply.github.com>
2020-11-06 14:35:17 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <h1 data-ns-test="project-name">music app</h1>
+      <p data-ns-test="project-description">stay relaxed</p>
+      <h1 data-ns-test="project-name">weather app</h1>
+      <p data-ns-test="project-description">know weather in your city</p>
+    </>
+  );
+};
 
 export default App;
